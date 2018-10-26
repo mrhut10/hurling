1417240f8c17ee1064ec8bf78ad6815696ea2f5d
mrhut10 <mrhut10@protonmail.com>
2018-10-27 03:46:51 +1100

    blogPostAdded

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,8 +7,49 @@
 const path = require('path')
 const slugify = require('slugify')
 
-const load_content = require('./load_blogs')
+//const load_content = require('./load_blogs')
 
 exports.onCreateNode = ({ node, actions }) => {
-  load_content.processBlogs(node, actions);
+  const { createNodeField } = actions
+  //load_content.processBlogs(node, actions);
+  if (node.internal.type === 'MarkdownRemark') {
+    if (node.fileAbsolutePath.match("/src/content/blogs/")) {
+      createNodeField({ node, name: 'instance', value: `blog` })
+      createNodeField({ node, name: 'slug', value: `/blog/${slugify(node.frontmatter.title)}` })
+
+    }
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+    {
+      allMarkdownRemark(filter: {fields: {instance: {eq: "blog"}}}) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+    
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+          createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/blog-post.js`),
+            context: {
+              // Data passed to context is available
+              // in page queries as GraphQL variables.
+              slug: node.fields.slug,
+            },
+          })
+        })
+        resolve()
+      })
+  })
 }
